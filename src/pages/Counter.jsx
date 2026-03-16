ef6b04ab884cd679425ddace52987f37e020c70f
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-16 12:08:32 +0530

    7th commit : {impliment timer bomb with sound effect, to useffect and settime, timeout used}

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,15 +1,28 @@
+import boomSound from "../assets/bomb.wav";
 import { useState, useEffect } from "react";
 
 const Counter = () => {
     const [value, setValue] = useState(0);
+    const [bomb, setBomb] = useState(false);
 
-    useEffect(()=>{
-      console.log("Inside UseEffect" + value);
+    useEffect(() => {
+        // console.log("Inside UseEffect" + value);
 
-      return(()=>{
-        console.log('cleanup' + value)
-      })
-    },[value]);
+        if (value === 0) {
+            setBomb(false);
+            return; // stop timer
+        }
+        setBomb(false);
+
+        const id = setTimeout(() => {
+            setBomb(true);
+
+            const audio = new Audio(boomSound);
+            audio.play();
+        }, value * 1000);
+
+        return () => clearTimeout(id); // console.log('cleanup' + value)
+    }, [value]);
 
     return (
         <div
@@ -17,8 +30,11 @@ const Counter = () => {
                 paddingTop: "120px",
                 background: "",
                 display: "flex",
+                flexDirection: "column",
                 alignContent: "center",
                 justifyContent: "center",
+                paddingLeft: "calc(50% - 100px)",
+                gap: "10px",
             }}
         >
             <div
@@ -53,6 +69,30 @@ const Counter = () => {
                         +
                     </button>
                 </div>
+            </div>
+
+            <div
+                style={{
+                    width: "200px",
+                    height: "200px",
+                    background: "black",
+                    color: "white",
+                    borderRadius: "8px",
+                    textAlign: "center",
+                    justifyContent: "center",
+                    alignContent: "center",
+                }}
+            >
+                {bomb && value ? (
+                    <span style={{ fontSize: "42px" }}>
+                        Boom..<span style={{ color: "red" }}>!!!</span>
+                    </span>
+                ) : (
+                    <span>
+                        {" "}
+                        Adjust Bomb timer <br /> in Counter
+                    </span>
+                )}
             </div>
         </div>
     );
